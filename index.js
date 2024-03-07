b712885f4dde9abe7178ab109c6e7079a48306bb
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-03-07 09:25:43 +0300

    add files

---
+++ index.js
@@ -0,0 +1,33 @@
+//QUESTION1
+let t = "Extravaganza";
+ console.log(t[8]);   
+ console.log(t[9]); 
+ console.log(t[10]); 
+ console.log(t[11]); 
+
+//QUESTION3
+const food = "The quick fox jumped over the dog";
+let q = "eat";
+let r = `The q${q}uick fox jumped over the dog`
+console.log(r);
+
+//QUESTION4
+let w = "The pupils are reading in the library";
+console.log(w.indexOf('are'));
+let s = "The child wa sitting on the table before it fell";
+console.log(s.indexOf('sitting'));
+
+//QUESTION5
+let x = "wonderful";
+y = x.toUpperCase();
+console.log(y);
+let a = "amazing";
+b = a.toLowerCase();
+console.log(b);
+let d = "UndERneath";
+e = d.toLowerCase();
+console.log(e);
+
+
+
+
